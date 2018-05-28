1ff6b78ac0bc12b5d5c5b9dd1e433dc08c16f19f
Caramba997 <39498668+Caramba997@users.noreply.github.com>
2018-05-28 21:58:13 +0200

    Revert "Hinweis von Eclipse entfernt"
    This reverts commit b6a3270315bd082208f56d7637b78fd57577802c.

--- src/de/uni_hamburg/informatik/swt/se2/mediathek/werkzeuge/subwerkzeuge/vormerkmedienauflister/VormerkMedienauflisterWerkzeug.java
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/werkzeuge/subwerkzeuge/vormerkmedienauflister/VormerkMedienauflisterWerkzeug.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import javax.swing.JPanel;
 import javax.swing.event.ListSelectionEvent;
